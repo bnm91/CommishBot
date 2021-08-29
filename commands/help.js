031bc58fddab6dcde18d8d3c400d916716a0acf0
Andrew Boorde <boorde.andrew@gmail.com>
2021-08-28 19:20:25 -0600

    weird

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,11 @@
-var { produceImmediateResponse } = require("../helpers/utils");
+var { produceResponseObjectForText } = require("../helpers/utils");
 var matcher = /!help/;
 
 function run(command, request) {
-  return produceImmediateResponse(
-    "Commands:\n" + "!pins\n" + "!all\n" + "!roll\n" + "!flip\n" + "!ping\n"
-  );
+  return {
+    text:
+      "Commands:\n" + "!pins\n" + "!all\n" + "!roll\n" + "!flip\n" + "!ping\n",
+  };
 }
 
 exports.run = run;
